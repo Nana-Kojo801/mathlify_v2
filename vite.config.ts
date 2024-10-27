e895e6030eeacfeab7422912ad9d9aa6cd45a044
Nana Kojo <nkabenyah@gmail.com>
2024-10-26 21:14:46 -0700

    fourth-commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@convex": path.resolve(__dirname, "./convex"),
-    },
-  },
   plugins: [react()],
 })
